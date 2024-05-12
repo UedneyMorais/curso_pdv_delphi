f47af126ff152e94c5fad7437c6ae5534b29a9fb
Uedney Morais <50669942+UedneyMorais@users.noreply.github.com>
2024-05-12 08:48:45 -0300

    Inttermediete commit

--- Login.pas
+++ Login.pas
@@ -3,22 +3,49 @@ unit Login;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.jpeg,
+  Vcl.Imaging.pngimage;
 
 type
-  TForm1 = class(TForm)
+  TFrmLogin = class(TForm)
+    Image1: TImage;
+    Panel1: TPanel;
+    pnlLogin: TPanel;
+    Image2: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure FormCanResize(Sender: TObject; var NewWidth, NewHeight: Integer;
+      var Resize: Boolean);
   private
     { Private declarations }
+    procedure centerPanel;
   public
     { Public declarations }
   end;
 
 var
-  Form1: TForm1;
+  FrmLogin: TFrmLogin;
 
 implementation
 
 {$R *.dfm}
+
+procedure TFrmLogin.centerPanel;
+begin
+  pnlLogin.Top := (Self.Height div 2) - (pnlLogin.Height div 2);
+  pnlLogin.Left := (Self.Width div 2) - (pnlLogin.Width div 2)
+end;
+
+procedure TFrmLogin.FormCanResize(Sender: TObject; var NewWidth,
+  NewHeight: Integer; var Resize: Boolean);
+begin
+      centerPanel;
+end;
+
+procedure TFrmLogin.FormCreate(Sender: TObject);
+begin
+      centerPanel;
+end;
 
 end.
